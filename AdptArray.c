2006b99edcbdbad65be8f196b0208cf905ce6618
EiTair <eytan1998@gmail.com>
2023-03-21 12:13:27 +0200

    add comments

--- AdptArray.c
+++ AdptArray.c
@@ -13,8 +13,10 @@ typedef struct AdptArray_ {
 
 PAdptArray CreateAdptArray(COPY_FUNC copy_func, DEL_FUNC del_func, PRINT_FUNC print_func) {
     PAdptArray pArr = (PAdptArray) malloc(sizeof(AdptArray));
+    // if failed initialize
     if (pArr == NULL)
         return NULL;
+    // up to preference, if 0 its granted extend on first set
     pArr->ArrSize = 0;
     pArr->pElemArr = NULL;
     pArr->copy_func = copy_func;
@@ -28,9 +30,11 @@ void DeleteAdptArray(PAdptArray pArr) {
     if (pArr == NULL)
         return;
     for (i = 0; i < pArr->ArrSize; ++i) {
+        // dont try delete null
         if ((pArr->pElemArr)[i] == NULL) continue;
         pArr->del_func((pArr->pElemArr)[i]);
     }
+    // free the array itself
     free(pArr->pElemArr);
     free(pArr);
 }
@@ -40,17 +44,21 @@ Result SetAdptArrayAt(PAdptArray pArr, int index, PElement element) {
     if (pArr == NULL)
         return FAIL;
 
+    // if need to resize
     if (index >= pArr->ArrSize) {
-
-// Extend Array
+        //allocate memory
         if ((newpElemArr = (PElement *) calloc((index + 1), sizeof(PElement))) == NULL)
+            //if failed dont continue
             return FAIL;
+        //copy memory
         memcpy(newpElemArr, pArr->pElemArr, (pArr->ArrSize) * sizeof(PElement));
+        //free the pre array
         free(pArr->pElemArr);
+        //change the pointer to new array
         pArr->pElemArr = newpElemArr;
     }
 
-    // Delete Previous Elem
+    // Delete Previous Elem and free it
     if ((pArr->pElemArr)[index] != NULL)
         pArr->del_func((pArr->pElemArr)[index]);
     (pArr->pElemArr)[index] = pArr->copy_func(element);
@@ -63,14 +71,18 @@ Result SetAdptArrayAt(PAdptArray pArr, int index, PElement element) {
 PElement GetAdptArrayAt(PAdptArray pArr, int index) {
     if (pArr == NULL)
         return NULL;
+    //check if index is in array size
     if (index >= pArr->ArrSize)
         return NULL;
+    //check if not null so wont try to copy null
     if (pArr->pElemArr[index] == NULL)
         return NULL;
+    //return COPY
     return pArr->copy_func(pArr->pElemArr[index]);
 }
 
 int GetAdptArraySize(PAdptArray pArr) {
+    // if not initialize so return -1
     if (pArr == NULL)
         return -1;
 
@@ -81,7 +93,9 @@ void PrintDB(PAdptArray pArr) {
     int i;
     if (pArr == NULL)
         return;
+
     for (i = 0; i < pArr->ArrSize; ++i) {
+        // dont try print null
         if ((pArr->pElemArr)[i] == NULL) continue;
         pArr->print_func((pArr->pElemArr)[i]);
     }
